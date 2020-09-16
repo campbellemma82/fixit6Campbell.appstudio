1ff2eff1a4f03ac792e78b87361b3f7bfe129031
Emma Campbell <eac46875@creighton.edu>
2020-09-16 16:32:44 -0500

    Marmaduke

---
+++ forms/pets/pets.js
@@ -0,0 +1,5 @@
+let petNames = ["Darcy", "Jack", "Jerry", "Marty", "Vito", "Vinny", "Riley", "Saddie", "Marge", "Sam"]
+let withMarmaduke = petNames
+withMarmaduke.push("Marmaduke")
+
+alert(withMarmaduke)
